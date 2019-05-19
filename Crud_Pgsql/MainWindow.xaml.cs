b191c15770e346f2237271edae4daed05b8c75f2
Vincent LAFARGUE <vincent.lafargue96@gmail.com>
2019-05-19 15:34:45 +0200

    Ajoutez des fichiers projet.

---
+++ Crud_Pgsql/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TutoDeveloppez;
+
+namespace Crud_Pgsql
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MyPersons per = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Refresh();
+        }
+        private void Refresh()
+        {
+            per = new MyPersons();
+            DataTable MyData = new DataTable();
+            MyData = per.SelectAllPerson();
+            TablePersonne.Items.Clear();
+
+            foreach (DataRow row in MyData.Rows)
+            {
+                var data = new ClTablePersonne { id = row[0].ToString(), nom = row[1].ToString(), prenom = row[2].ToString() };
+                TablePersonne.Items.Add(data);
+            }
+        
+        }
+        private void ClicRefreshPerson(object sender, RoutedEventArgs e)
+        {
+            Refresh();
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ClicAddPerson(object sender, RoutedEventArgs e)
+        {
+            per = new MyPersons();
+            TablePersonne.Items.Clear();
+            //Appel de la méthode InsertPsersons créee dans
+            per.InsertPersons(TextName.Text, TextPrenom.Text, TextTelephone.Text, TextAdresse.Text);
+            Refresh();
+        }
+        public class ClTablePersonne
+        {
+            public string id { get; set; }
+            public string nom { get; set; }
+            public string prenom { get; set; }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void ClicDelete(object sender, RoutedEventArgs e)
+        {
+            if (TablePersonne.SelectedIndex != -1)
+            {
+                //string myId = TablePersonne.SelectedItems = "{Binding Path=id}";
+                var data = new ClTablePersonne {};
+                string myId= (this.DataContext as ClTablePersonne).Tostring;
+                int id = int.Parse(myId);
+                per.DeletePersonneById(id);
+            }
+            else
+            {
+                MessageBox.Show("Vous devez sélectionner une ligne dans le tableau pour pouvoir supprimer un enregistrement !");   
+            }
+        }
+    }
+}
---
+++ Crud_Pgsql/MyPersons.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+
+//On inclue la librairie
+using Npgsql;
+using NpgsqlTypes;
+//Fin
+using System.Linq;
+using System.Data;
+using System.Text;
+
+namespace TutoDeveloppez
+{
+    //Réalisé par José Landry BOGBE le 09/07/2014 a 10 heures 39 minutes
+    public class MyPersons
+    {
+        string Conx = "Server=localhost;Port=5432;Database=db_crud;User Id=postgres;Password=a;";
+        NpgsqlCommand MyCmd = null;
+        NpgsqlConnection MyCnx = null;
+
+        public void InsertPersons(string nom, string prenom, string tel, string adresse)
+        {
+            MyCnx = new NpgsqlConnection(Conx);
+            string insert = "INSERT INTO \"Personne\"(id,nom,prenom,telephone,adresse) values(DEFAULT,:nom,:prenom,:tel,:adresse)";
+            //La valeur DEFAULT parce que la propriété id est auto incrémenté
+            MyCnx.Open();
+            MyCmd = new NpgsqlCommand(insert, MyCnx);
+
+            //Définition  et ajout des paramètres 
+
+            MyCmd.Parameters.Add(new NpgsqlParameter("nom", NpgsqlDbType.Varchar)).Value = nom;
+            MyCmd.Parameters.Add(new NpgsqlParameter("prenom", NpgsqlDbType.Varchar)).Value = prenom;
+            MyCmd.Parameters.Add(new NpgsqlParameter("tel", NpgsqlDbType.Varchar)).Value = tel;
+            MyCmd.Parameters.Add(new NpgsqlParameter("adresse", NpgsqlDbType.Varchar)).Value = adresse;
+
+            MyCmd.ExecuteNonQuery(); //Exécution
+            MyCnx.Close();
+        }
+
+        public void UpdatePersons(int id, string nom, string prenom, string tel, string adresse)
+        {
+            MyCnx = new NpgsqlConnection(Conx); //Instanciation
+            string update = "UPDATE  \"Personne\"  SET nom =:pnom ,prenom=:pprenom,telephone=:ptel,adresse=:padresse WHERE(id=:pid);";
+            MyCnx.Open();
+
+            MyCmd = new NpgsqlCommand(update, MyCnx);
+
+            //Définition  et ajout des paramètres 
+            MyCmd.Parameters.Add(new NpgsqlParameter("pid", NpgsqlDbType.Varchar)).Value = id;
+            MyCmd.Parameters.Add(new NpgsqlParameter("pnom", NpgsqlDbType.Varchar)).Value = nom;
+            MyCmd.Parameters.Add(new NpgsqlParameter("pprenom", NpgsqlDbType.Varchar)).Value = prenom;
+            MyCmd.Parameters.Add(new NpgsqlParameter("ptel", NpgsqlDbType.Varchar)).Value = tel;
+            MyCmd.Parameters.Add(new NpgsqlParameter("padresse", NpgsqlDbType.Varchar)).Value = adresse;
+
+            MyCmd.ExecuteNonQuery();//Exécution
+            MyCnx.Close();
+        }
+
+        public DataTable SelectAllPerson()
+        {
+            DataTable MyData = new DataTable();
+            NpgsqlDataAdapter da;
+            MyCnx = new NpgsqlConnection(Conx);
+            MyCnx.Open();
+            string select = "SELECT * FROM \"Personne\"";
+            MyCmd = new NpgsqlCommand(select, MyCnx);
+            da = new NpgsqlDataAdapter(MyCmd);
+            da.Fill(MyData);
+            MyCnx.Close();
+            return MyData;
+        }
+
+        public void DeletePersonneById(int idpersonne)
+        {
+            MyCnx = new NpgsqlConnection(Conx);
+            string delete = "DELETE FROM \"Personne\" WHERE(id=:pid)";
+
+            MyCnx.Open();
+            MyCmd = new NpgsqlCommand(delete, MyCnx);
+            MyCmd.Parameters.Add(new NpgsqlParameter("pid", NpgsqlDbType.Integer)).Value = idpersonne;
+            MyCmd.ExecuteNonQuery();
+            MyCnx.Close();
+        }
+    }
+}//Fin
